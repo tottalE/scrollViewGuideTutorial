692ac50c904206c77bf2ef0f1781a4ad56c80160
jin <pearlisrich@naver.com>
2022-11-01 13:29:53 +0900

    feat: set first start of project

--- scrollViewGuide/ViewController.swift
+++ scrollViewGuide/ViewController.swift
@@ -9,31 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let contentView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = UIColor.green
-        return view
-    }()
+    private let contentView: UIStackView = UIStackView()
     
     private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.addSubview(contentView)
         return scrollView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        fillStackView()
     }
-
+    
+    // set up stackview to vertical
+    private func setupStackView() {
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.axis = .vertical
+        contentView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor).isActive = true
+    }
+    
+    // fillstackview with colors.
+    private func fillStackView() {
+        let colorArray = [UIColor.blue, .red, .yellow, .purple, .green, .black, .orange, .gray]
+        for color in colorArray {
+            let elementView = UIView()
+            elementView.backgroundColor = color
+            elementView.translatesAutoresizingMaskIntoConstraints = false
+            elementView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+            contentView.addArrangedSubview(elementView)
+        }
+    }
+    
     func setupScrollViewToSuperView() {
-        
+        // you can write code here.
     }
     
     func setupContentViewToScrollView() {
-        
+        // you can write code here.
     }
+    
+    
+    
 }
 
